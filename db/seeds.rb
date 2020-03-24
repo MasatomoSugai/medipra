b829a34a5542ec12c8f2dca9937e53c1f8e90942
Masatomo Sugai <tech.masatomo.s@gmail.com>
2020-03-24 12:37:52 +0900

    fix seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,24 +9,43 @@ require "csv"
 date = "2019-11-16"
 i = 1
 
-CSV.foreach('db/medipra_20200401a.csv') do |info|
+CSV.foreach('db/yakka20200401.csv') do |info|
   Product.create!(
-    category: info[0],
-    YJ_code: info[1],
-    ingredients: info[2],
-    standard: info[3],
-    Kyokuhou: info[4],
-    narcotic: info[5],
-    other: info[6],
-    name: info[7],
-    company: info[8],
-    generic: info[9],
-    original: info[10],
-    same_generic: info[11],
-    price: info[12],
-    transitional_time_limit: info[13],
-    spare: info[14],
-    created_at: date,
-    updated_at: date
+    price: info[11],
+    change_category: info[0],
+    master_type: info[1],
+    pharmaceutical_code: info[2],
+    kanji_significant_digits: info[3],
+    kanji_name: info[4],
+    kana_significant_digits: info[5],
+    kana_name: info[6],
+    unit_code: info[7],
+    unit_significant_digits: info[8],
+    unit_name: info[9],
+    price_type: info[10],
+    spare: info[12],
+    n_p_s_p_drugs: info[13],
+    nerve_destroyer: info[14],
+    biologics: info[15],
+    generic: info[16],
+    spare_2: info[17],
+    dental_drugs: info[18],
+    contrast_agent: info[19],
+    injection_volume: info[20],
+    listing_type: info[21],
+    product_name_relations: info[22],
+    old_price_type: info[23],
+    old_price: info[24],
+    name_change_category: info[25],
+    kana_name_change_category: info[26],
+    dosage_form: info[27],
+    spare_3: info[28],
+    changed_date: info[29],
+    abolition_date: info[30],
+    standard_code: info[31],
+    order_number: info[32],
+    expiration_date: info[33],
+    standard_name: info[34]
+
   )
 end
