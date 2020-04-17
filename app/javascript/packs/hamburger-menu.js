0a5f3a6261a7ad94ba0a82beb2ce59455fd04612
Masatomo Sugai <tech.masatomo.s@gmail.com>
2020-04-18 08:28:40 +0900

    create hamburger menu js

--- app/javascript/packs/hamburger-menu.js
+++ app/javascript/packs/hamburger-menu.js
@@ -1,5 +1,13 @@
+
 $(function(){
   $(document).ready(function() {
-    $('.drawer').drawer();
+    $('.header_button').click(function(){
+      $('.header_button__menu').stop(true, true).slideDown('fast');
+    });
+    $('#menu_close').click(function(e){
+      $('.header_button__menu').stop(true, true).slideUp('fast');
+      e.stopPropagation();
+      // ↑↑親要素もクリックされたことになるのを防ぐために必要
+    });
   });
 });
