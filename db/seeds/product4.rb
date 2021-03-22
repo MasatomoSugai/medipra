a4ad5752f9e70ecfd8bc6b4485f10ddd74172c37
Masatomo Sugai <tech.masatomo.s@gmail.com>
2021-03-22 13:40:23 +0900

    fix seed porudt1~4

--- db/seeds/product4.rb
+++ db/seeds/product4.rb
@@ -3,7 +3,7 @@ require "csv"
 date = "2020-04-01"
 i = 1
 
-CSV.foreach('db/medipra20201210ad.csv') do |info|
+CSV.foreach('db/medipra20210310ad.csv') do |info|
   Product.create!(
     price: info[11],
     change_category: info[0],
