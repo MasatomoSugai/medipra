347165d70c57c0b2418a0996c65e293177b74127
Masatomo Sugai <tech.masatomo.s@gmail.com>
2021-06-07 11:21:36 +0900

    seedコマンドの変更

--- db/seeds/product2.rb
+++ db/seeds/product2.rb
@@ -3,7 +3,7 @@ require "csv"
 date = "2020-04-01"
 i = 1
 
-CSV.foreach('db/medipra20210310ab.csv') do |info|
+CSV.foreach('db/medipra20210601ab.csv') do |info|
   Product.create!(
     price: info[11],
     change_category: info[0],
